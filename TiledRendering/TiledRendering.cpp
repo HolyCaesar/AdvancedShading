dd7c4aabc63f88224412fc7c3af6e9649ce9de23
HolyKaisar <caesardzh@gmail.com>
2020-03-31 20:46:27 -0500

    Used a default heap to upload vertex to a GPU buffer instead of using upload heap which could marshalled over every time the GPU needs it. Improved some performance.

--- TiledRendering/TiledRendering.cpp
+++ TiledRendering/TiledRendering.cpp
@@ -308,6 +308,7 @@ void TiledRendering::LoadAssets()
 	ThrowIfFailed(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator[m_frameIndex].Get(), m_pipelineState.Get(), IID_PPV_ARGS(&m_commandList)));
 
 	// Create the vertex buffer.
+	ComPtr<ID3D12Resource> vertexUploadHeap;
 	{
 		// Define the geometry for a triangle.
 		Vertex triangleVertices[] =
@@ -317,26 +318,59 @@ void TiledRendering::LoadAssets()
 			{ { -0.25f, -0.25f * m_aspectRatio, 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f }, {0.0f, 1.0f } }
 		};
 
+
 		const UINT vertexBufferSize = sizeof(triangleVertices);
 
-		// Note: using upload heaps to transfer static data like vert buffers is not 
-		// recommended. Every time the GPU needs it, the upload heap will be marshalled 
-		// over. Please read up on Default Heap usage. An upload heap is used here for 
-		// code simplicity and because there are very few verts to actually transfer.
 		ThrowIfFailed(m_device->CreateCommittedResource(
-			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
 			D3D12_HEAP_FLAG_NONE,
 			&CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize),
-			D3D12_RESOURCE_STATE_GENERIC_READ,
+			D3D12_RESOURCE_STATE_COPY_DEST,
 			nullptr,
 			IID_PPV_ARGS(&m_vertexBuffer)));
 
-		// Copy the triangle data to the vertex buffer.
-		UINT8* pVertexDataBegin;
-		CD3DX12_RANGE readRange(0, 0);        // We do not intend to read from this resource on the CPU.
-		ThrowIfFailed(m_vertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)));
-		memcpy(pVertexDataBegin, triangleVertices, sizeof(triangleVertices));
-		m_vertexBuffer->Unmap(0, nullptr);
+		const UINT64 vertexUploadBufferSize = GetRequiredIntermediateSize(m_vertexBuffer.Get(), 0, 1);
+
+		ThrowIfFailed(m_device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+			D3D12_HEAP_FLAG_NONE,
+			&CD3DX12_RESOURCE_DESC::Buffer(vertexUploadBufferSize),
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&vertexUploadHeap)));
+
+
+
+
+		D3D12_SUBRESOURCE_DATA vertexData = {};
+		vertexData.pData = reinterpret_cast<BYTE*>(triangleVertices);
+		vertexData.RowPitch = 3 * sizeof(triangleVertices) / sizeof(triangleVertices[0]);
+		vertexData.SlicePitch = vertexData.RowPitch;
+
+		UpdateSubresources(m_commandList.Get(), m_vertexBuffer.Get(), vertexUploadHeap.Get(), 0, 0, 1, &vertexData);
+		m_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(m_vertexBuffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER));
+
+
+
+		//const UINT vertexBufferSize = sizeof(triangleVertices);
+		//// Note: using upload heaps to transfer static data like vert buffers is not 
+		//// recommended. Every time the GPU needs it, the upload heap will be marshalled 
+		//// over. Please read up on Default Heap usage. An upload heap is used here for 
+		//// code simplicity and because there are very few verts to actually transfer.
+		//ThrowIfFailed(m_device->CreateCommittedResource(
+		//	&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		//	D3D12_HEAP_FLAG_NONE,
+		//	&CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize),
+		//	D3D12_RESOURCE_STATE_GENERIC_READ,
+		//	nullptr,
+		//	IID_PPV_ARGS(&m_vertexBuffer)));
+
+		//// Copy the triangle data to the vertex buffer.
+		//UINT8* pVertexDataBegin;
+		//CD3DX12_RANGE readRange(0, 0);        // We do not intend to read from this resource on the CPU.
+		//ThrowIfFailed(m_vertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)));
+		//memcpy(pVertexDataBegin, triangleVertices, sizeof(triangleVertices));
+		//m_vertexBuffer->Unmap(0, nullptr);
 
 		// Initialize the vertex buffer view.
 		m_vertexBufferView.BufferLocation = m_vertexBuffer->GetGPUVirtualAddress();
