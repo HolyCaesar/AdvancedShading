754593e604917f13c78a3a86dbedce805ee09d37
DESKTOP-CFVHQTV\holyk <caesardzh@gmai.com>
2020-10-24 18:52:50 -0400

    Added a deferred rendering draft.

--- TiledRendering/RenderTechnique.cpp
+++ TiledRendering/RenderTechnique.cpp
@@ -1,3 +1,28 @@
 #include "RenderTechnique.h"
+#include "RenderPass.h"
+#include "CommandContext.h" 
 
+using namespace std;
+
+RenderTechnique::RenderTechnique()
+{
+
+}
+
+RenderTechnique::~RenderTechnique()
+{
+
+}
+
+uint64_t RenderTechnique::AddPass(shared_ptr<RenderPass> pass)
+{
+	uint64_t idx = static_cast<uint64_t>(m_renderPassList.size());
+	m_renderPassList.push_back(pass);
+	return idx;
+}
+
+shared_ptr<RenderPass> RenderTechnique::GetPass(uint64_t passIdx)
+{
+	return m_renderPassList[passIdx];
+}
 
--- TiledRendering/RenderTechnique.h
+++ TiledRendering/RenderTechnique.h
@@ -3,12 +3,13 @@
 #include <string>
 
 class CommandContext;
+class RenderPass;
 
 class RenderTechnique : public Object
 {
 public:
 	RenderTechnique();
-	~RenderTechnique();
+	virtual ~RenderTechnique();
 
 	virtual void Init(std::string name, uint64_t width, uint64_t height) = 0;
 
@@ -16,11 +17,13 @@ public:
 
 	virtual void Destroy() = 0;
 
+	virtual uint64_t AddPass(std::shared_ptr<RenderPass> pass);
+	std::shared_ptr<RenderPass> GetPass(uint64_t passIdx);
+
 private:
 	std::string m_name;
 
-	// TODO: do we need a pass class since a render technique may need multiple passes
-	// or I can let each render technique to control how many passes they need.
+	std::vector<std::shared_ptr<RenderPass>> m_renderPassList;
 
 private:
 	RenderTechnique(const RenderTechnique& copy) = delete;
@@ -28,3 +31,22 @@ private:
 	RenderTechnique& operator=(const RenderTechnique& other) = delete;
 };
 
+class DeferredRendering : public RenderTechnique
+{
+public:
+	DeferredRendering();
+	virtual ~DeferredRendering();
+
+	void Init(std::string name, uint64_t width, uint64_t height);
+
+	void Render(CommandContext& Context);
+
+	void Destroy();
+
+private:
+
+private:
+	DeferredRendering(const DeferredRendering& copy) = delete;
+	DeferredRendering(DeferredRendering&& copy) = delete;
+	DeferredRendering& operator=(const DeferredRendering& other) = delete;
+};
